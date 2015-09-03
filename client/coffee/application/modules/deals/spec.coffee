9ba753357d546e07189c61a1ed65e34816ab8b8c
Denis Savenok <denissavenok@gmail.com>
2015-09-03 13:20:45 +0400

    dealsListItem added to templates; deals module spec populated

--- client/coffee/application/modules/deals/spec.coffee
+++ client/coffee/application/modules/deals/spec.coffee
@@ -3,4 +3,26 @@ define
         'wire/debug'
     ]
 
-    showDealsList: ->
+    dealsCollection:
+        create: 'application/modules/deals/collections/deals'
+        ready:
+            fetch: {}
+
+    dealsList:
+        create: 'blocks/views/list/index'
+        properties:
+            collection          : {$ref: 'dealsCollection'}
+            channel             : {$ref: 'channel'}
+            entity              : "deal"
+        ready:
+            setChildTemplate: {$ref: 'hbs!templates/dealsListItem' }
+
+    dealsController:
+        create: 'application/modules/deals/controller'
+        properties:
+            collection          : {$ref: 'dealsCollection'}
+            channel             : {$ref: 'channel'}
+            list                : {$ref: 'dealsList'}
+
+    activateById: (id) ->
+        @dealsController.activateById id
--- client/js/application/modules/deals/spec.js
+++ client/js/application/modules/deals/spec.js
@@ -1,4 +1,43 @@
 define({
   $plugins: ['wire/debug'],
-  showDealsList: function() {}
+  dealsCollection: {
+    create: 'application/modules/deals/collections/deals',
+    ready: {
+      fetch: {}
+    }
+  },
+  dealsList: {
+    create: 'blocks/views/list/index',
+    properties: {
+      collection: {
+        $ref: 'dealsCollection'
+      },
+      channel: {
+        $ref: 'channel'
+      },
+      entity: "deal"
+    },
+    ready: {
+      setChildTemplate: {
+        $ref: 'hbs!templates/dealsListItem'
+      }
+    }
+  },
+  dealsController: {
+    create: 'application/modules/deals/controller',
+    properties: {
+      collection: {
+        $ref: 'dealsCollection'
+      },
+      channel: {
+        $ref: 'channel'
+      },
+      list: {
+        $ref: 'dealsList'
+      }
+    }
+  },
+  activateById: function(id) {
+    return this.dealsController.activateById(id);
+  }
 });

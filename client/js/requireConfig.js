d736c511f76e770f4a2dacb335426f33e560928c
Denis Savenok <denissavenok@gmail.com>
2015-09-06 17:39:46 +0400

    reverted back vendors configuration

--- client/js/requireConfig.js
+++ client/js/requireConfig.js
@@ -1,53 +1,54 @@
 require.config({
   baseUrl: "/js",
-  paths: {
-    "text": "lib/text"
-  },
   packages: [
     {
       name: "backbone",
       main: "backbone",
-      location: "../../vendors/backbone"
+      location: "vendors/backbone"
     }, {
       name: "backbone.radio",
       main: "backbone.radio",
-      location: "../../vendors/backbone.radio/build"
+      location: "vendors/backbone.radio/build"
     }, {
       name: "marionette",
       main: "backbone.marionette",
-      location: "../../vendors/marionette/lib"
+      location: "vendors/marionette/lib"
     }, {
       name: "underscore",
       main: "underscore",
-      location: "../../vendors/underscore"
+      location: "vendors/underscore"
     }, {
       name: "jquery",
       main: "jquery",
-      location: "../../vendors/jquery/dist"
+      location: "vendors/jquery/dist"
     }, {
       name: "handlebars",
       main: "handlebars",
-      location: "../../vendors/handlebars"
+      location: "vendors/handlebars"
+    }, {
+      name: "text",
+      main: "text",
+      location: "vendors/text"
     }, {
       name: "hbs",
       main: "hbs",
-      location: "../../vendors/requirejs-hbs"
+      location: "vendors/requirejs-hbs"
     }, {
       name: "wire",
       main: "wire",
-      location: "../../vendors/wire"
+      location: "vendors/wire"
     }, {
       name: "when",
       main: "when",
-      location: "../../vendors/when"
+      location: "vendors/when"
     }, {
       name: "meld",
       main: "meld",
-      location: "../../vendors/meld"
+      location: "vendors/meld"
     }, {
       name: "moment",
       main: "moment",
-      location: "../../vendors/moment"
+      location: "vendors/moment"
     }
   ],
   shim: {},
